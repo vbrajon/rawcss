9ba996ce00dc6a72629bc571936fc9282e8ea86c
Valentin Brajon <valentin.brajon@neoxam.com>
2022-01-30 23:53:24 +0100

    feat: add tailwind typo/form + add detail/popup/tooltip

--- vite.config.ts
+++ vite.config.ts
@@ -26,46 +26,29 @@ export default defineConfig({
         },
         {
           name: 'preset-prose',
-          rules: [],
         },
         {
           name: 'preset-cut',
           theme: {
-            fontFamily: {
-              sans: ['Inter'],
-              mono: ['Hack'],
+            fontSize: {
+              '2xs': ['0.625rem', '1rem'],
             },
           },
           shortcuts: [
-            [/^title-(\d)$/, ([, n = 1]) => [, 'text-3xl', 'text-2xl', 'text-xl'][n] || ''],
-            [/^text-(\d)$/, ([, n = 1]) => [, 'text', 'text-sm', 'text-xs'][n] || ''],
-            ['link', `cursor-pointer inline-flex font-medium underline decoration-2 decoration-indigo hover:text-indigo`],
             [
-              'button',
-              `cursor-pointer inline-flex font-medium px-4 py-2 border shadow-sm font-medium rounded-md bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`,
+              /^title-(\d)$/,
+              ([, n = 1]) => [, 'text-2xl sm:text-3xl font-extrabold', 'text-2xl sm:text-xl font-bold', 'text-lg font-semibold', 'font-semibold'][n] || '',
             ],
+            [/^text-(\d)$/, ([, n = 1]) => [, 'text-base', 'text-sm', 'text-xs', 'text-2xs'][n] || ''],
+            ['link', `cursor-pointer inline-flex font-medium underline decoration-2 decoration-indigo hover:text-indigo`],
+            ['button', `cursor-pointer inline-flex font-medium py-1 px-2 border border-2 border-indigo rounded bg-white hover:text-indigo`],
             [
               /^button(-.*)?$/,
-              ([, c = '-indigo']) =>
-                `cursor-pointer inline-flex px-4 py-1.5 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg${c}-600 hover:bg${c}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring${c}-500`,
+              ([, c = '-indigo']) => `cursor-pointer inline-flex font-medium py-1 px-2 border border-2 border${c} rounded bg-white hover:text${c}`,
             ],
             [/^badge(-.*)?$/, ([, c = '-indigo']) => `inline-flex px-2 py-0.5 rounded font-medium bg${c}-100 text${c}-800`],
           ],
         },
-        // {
-        //   name: 'preset-line-clamp',
-        //   rules: [
-        //     [
-        //       /^line-clamp-(\d+)$/,
-        //       ([, d]) => ({
-        //         overflow: 'hidden',
-        //         display: '-webkit-box',
-        //         '-webkit-box-orient': 'vertical',
-        //         '-webkit-line-clamp': 1,
-        //       }),
-        //     ],
-        //   ],
-        // },
       ],
     }),
   ],
