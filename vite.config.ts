04e7bea4383bb06a9105e26810c34a022288e6da
Valentin Brajon <valentin.brajon@neoxam.com>
2022-02-25 07:40:18 +0100

    feat: add on demand css file load

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import Unocss from 'unocss/vite'
-import { presetUno, presetIcons } from 'unocss'
+import { presetUno, presetIcons, presetTypography } from 'unocss'
+
+import fs from 'fs'
+const inter = fs.readFileSync('./tailwind-inter.css', 'utf8')
+const form = fs.readFileSync('./tailwind-form.css', 'utf8')
+const cut = fs.readFileSync('./cut.css', 'utf8')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,24 +17,17 @@ export default defineConfig({
   },
   plugins: [
     Unocss({
-      theme: {
-        fontFamily: {
-          sans: ['MarkPro'],
-          mono: ['Hack'],
-        },
-      },
       presets: [
-        presetUno(),
-        presetIcons(),
-        {
-          name: 'preset-form',
-        },
-        {
-          name: 'preset-prose',
-        },
+        presetUno(), //* .m-4 .p-4 .w-full .rounded-lg
+        presetIcons(), //* .i-feather-activity
+        presetTypography(), //* .prose dark:prose-invert prose-img:rounded-xl
         {
           name: 'preset-cut',
           theme: {
+            // fontFamily: {
+            //   sans: ['MarkPro'],
+            //   mono: ['Hack'],
+            // },
             fontSize: {
               '2xs': ['0.625rem', '1rem'],
             },
@@ -47,6 +45,21 @@ export default defineConfig({
               ([, c = '-indigo']) => `cursor-pointer inline-flex font-medium py-1 px-2 border border-2 border${c} rounded bg-white hover:text${c}`,
             ],
             [/^badge(-.*)?$/, ([, c = '-indigo']) => `inline-flex px-2 py-0.5 rounded font-medium bg${c}-100 text${c}-800`],
+          ],
+          layers: {
+            reset: -100,
+          },
+          rules: [
+            [/^reset$/, () => `@import '@unocss/reset/tailwind.css';*,::before,::after{border-color:#e5e7eb;}`, { layer: 'reset' }],
+            [/^inter$/, () => inter, { layer: 'reset' }],
+            [/^form$/, () => form, { layer: 'reset' }],
+            [/^cut$/, () => cut, { layer: 'reset' }],
+          ],
+        },
+        {
+          name: 'preset-debug',
+          shortcuts: [
+            ['shortcutcut', `link button badge`], //* shortcuts can be nested
           ],
         },
       ],
